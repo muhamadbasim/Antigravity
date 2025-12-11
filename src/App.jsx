32f25b5b8e7e0b0f894567e16ec97c3a03e63c6b
Muhamad Basim <muhamadbasim@gmail.com>
2025-12-11 21:07:37 +0700

    Debug: Disable 3D Experience, show only Overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Experience from './components/Experience'
+// import Experience from './components/Experience'
 import Overlay from './components/Overlay'
 
 function App() {
   return (
     <>
-      <Experience />
-      <Overlay />
+      {/* <Experience /> */}
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: '#111' }}>
+        <Overlay />
+      </div>
     </>
   )
 }
